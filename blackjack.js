d6018ea38e031ac08e7b655ed2bdbb76b0ac8d1b
eJanuel <januel.etienne@gmail.com>
2017-11-13 16:25:29 +0100

    all numbers are given

---
+++ blackjack.js
@@ -0,0 +1,10 @@
+function getRandomArbitrary(){
+
+}
+
+function blackJack(){
+  var randomValue = Math.floor(Math.random()*11);
+  var userValue = document.getElementById('userValue').value;
+  console.log(userValue);
+  console.log(randomValue);
+}
