1237d0d09f891efa3ac7e0c648e8a2ec18d6a803
eJanuel <januel.etienne@gmail.com>
2017-11-13 17:12:53 +0100

    Etape 1 terminée et début design

--- blackjack.js
+++ blackjack.js
@@ -1,10 +1,13 @@
-function getRandomArbitrary(){
-
-}
-
 function blackJack(){
   var randomValue = Math.floor(Math.random()*11);
   var userValue = document.getElementById('userValue').value;
   console.log(userValue);
   console.log(randomValue);
+
+  if (randomValue < userValue){
+    document.getElementById('result').innerHTML = 'Vous avez gagné';
+  } else {
+    document.getElementById('result').innerHTML = 'Vous avez perdu';
+  }
+
 }
